2c15f867fc7079d517f07c5c2db1592420911408
ronen.goren <ronen.goren@gmail.com>
2018-09-12 18:53:08 -0400

    connect form to redux

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
-import { database } from '../firebase'
 import _ from 'lodash';
+import { connect } from 'react-redux';
+import { getNotes, saveNote } from '../actions/notesAction'
+
 
 
 class App extends Component {
@@ -8,8 +10,7 @@ class App extends Component {
     super(props)
     this.state = {
       title: '',
-      body: '',
-      notes: {}
+      body: ''
     };
     this.handleChange = this.handleChange.bind(this)
     this.handleSubmit = this.handleSubmit.bind(this)
@@ -18,9 +19,7 @@ class App extends Component {
   }
 
   componentDidMount() {
-    database.on('value', (snapshot) => {
-      this.setState({ notes: snapshot.val() })
-    })
+    this.props.getNotes()
   }
 
   handleChange(e) {
@@ -35,7 +34,7 @@ class App extends Component {
       title: this.state.title,
       body: this.state.body
     }
-    database.push(note);
+    this.props.saveNote(note)
     this.setState({
       title: '',
       body: ''
@@ -43,7 +42,7 @@ class App extends Component {
   }
 
   renderNotes() {
-    return _.map(this.state.notes, (note, key) => {
+    return _.map(this.props.notes, (note, key) => {
       return (
         <div key={key}>
           <h2>{note.title}</h2>
@@ -97,4 +96,12 @@ class App extends Component {
   }
 }
 
-export default App;
+
+
+function mapStateToProps(state, ownProps) {
+  return {
+    notes: state.notes
+  }
+}
+
+export default connect(mapStateToProps, { getNotes, saveNote })(App);
--- src/index.js
+++ src/index.js
@@ -4,10 +4,16 @@ import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import rootReducer from './reducers'
-const store = createStore(composeWithDevTools());
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
+
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>, document.getElementById('root'));
 registerServiceWorker();
