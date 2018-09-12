abdcf405b6596d357ae995aea03e5499072d5abd
ronen.goren <ronen.goren@gmail.com>
2018-09-12 18:22:23 -0400

    create initial reducer and action

--- src/index.js
+++ src/index.js
@@ -2,6 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import rootReducer from './reducers'
+const store = createStore(composeWithDevTools());
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
